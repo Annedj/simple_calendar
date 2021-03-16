540037d0ce166074f853feaaf4627f64e0a6dbeb
Anne de Joly <anne.dejoly@edhec.com>
2021-03-16 15:53:08 +0000

    Add weekly wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_filter :set_time_zone, if: :user_signed_in?
-
-  private
-
-  def set_time_zone
-    Time.zone = current_user.time_zone
-  end
 end
