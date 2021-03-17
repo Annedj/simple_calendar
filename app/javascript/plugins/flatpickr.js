cb72bb7761b7389161f9dc14a3b2ba877604bf08
Anne de Joly <anne.dejoly@edhec.com>
2021-03-17 20:00:41 +0000

    Add time to events and improve style

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,6 +4,7 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 const initRangePickr = () => {
   flatpickr("#range_start", {
     altInput: true,
+    "enableTime": true,
     plugins: [new rangePlugin({ input: "#range_end"})]
   });
 }
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,16 @@
 class Meeting < ApplicationRecord
   after_create_commit :set_end_time
+  validates :start_time, :end_time, presence: true
+
+  default_scope -> { order(:start_time) }
+
+  def time
+    "#{start_time&.strftime('%I:%M %p')} - #{end_time&.strftime('%I:%M %p')}"
+  end
+
+  def multi_days?
+    (end_time&.to_date - start_time&.to_date).to_i > 1
+  end
 
   private
 
