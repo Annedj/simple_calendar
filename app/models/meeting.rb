4ded4fd4bbb4197d697054ddff646277cafd897c
Anne de Joly <anne.dejoly@edhec.com>
2021-03-18 09:19:31 +0000

    Remove safe operator

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,10 +4,10 @@ class Meeting < ApplicationRecord
   default_scope -> { order(:start_time) }
 
   def time
-    "#{start_time&.strftime('%I:%M %p')} - #{end_time&.strftime('%I:%M %p')}"
+    "#{start_time.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
   end
 
   def multi_days?
-    (end_time&.to_date - start_time&.to_date).to_i >= 1
+    (end_time.to_date - start_time.to_date).to_i >= 1
   end
 end
